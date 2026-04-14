5ce5dd963cb38933a6d67a4ad471e101daec7533
tomas <tomas.sabino@outlook.com>
2026-04-14 17:08:36 +0100

    Add simple webserver

--- main.tf
+++ main.tf
@@ -24,11 +24,35 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+resource "aws_security_group" "instance" {
+    name = "terraform-lab-instance-sg"
+
+    ingress {
+        from_port   = 8080
+        to_port     = 8080
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
 resource "aws_instance" "example" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  vpc_security_group_ids = [ aws_security_group.instance.id ]
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  user_data_replace_on_change = true
   tags = {
-    Name = "terraform-lab-single-instance"
+    Name = "terraform-lab-single-web-instance"
   }
 }
-
