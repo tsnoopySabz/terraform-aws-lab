888e5063f8a1e967958ea02575940f1b342abb57
tomas <tomas.sabino@outlook.com>
2026-04-15 17:18:47 +0100

    Add Application Load Balancer and update security groups for autoscaling

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group" "instance" {
         from_port   = var.server_port
         to_port     = var.server_port
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        security_groups = [aws_security_group.alb.id]
     }
   
     egress {
@@ -52,6 +52,26 @@ resource "aws_security_group" "instance" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+resource "aws_security_group" "alb" {
+    name = "terraform-lab-alb-sg"
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  
+}
+
 
 resource "aws_launch_template" "example"{
     image_id = data.aws_ami.ubuntu.id
@@ -75,6 +95,8 @@ resource "aws_autoscaling_group" "example" {
       version = "$Latest"
     }
     vpc_zone_identifier = data.aws_subnets.default.ids
+    target_group_arns = [aws_lb_target_group.asg.arn]
+    health_check_type = "ELB"
     min_size = 2
     max_size = 4
 
@@ -84,4 +106,61 @@ resource "aws_autoscaling_group" "example" {
       propagate_at_launch = true
     }
   
+}
+
+resource "aws_lb" "example" {
+    name = "terraform-lb-example"
+    load_balancer_type = "application"
+    security_groups = [aws_security_group.alb.id]
+    subnets = data.aws_subnets.default.ids
+  
+}
+
+resource "aws_lb_target_group" "asg" {
+    name = "terraform-asg-example"
+    port = var.server_port
+    protocol = "HTTP"
+    vpc_id = data.aws_vpc.default.id
+
+    health_check {
+      path = "/"
+      protocol = "HTTP"
+      matcher = "200"
+      interval = 15
+      timeout = 3
+      healthy_threshold = 2
+      unhealthy_threshold = 2
+    }
+}
+
+resource "aws_lb_listener" "http"{
+    load_balancer_arn = aws_lb.example.arn
+    port = 80
+    protocol = "HTTP" 
+
+    default_action {
+        type = "fixed-response"
+        fixed_response {
+          content_type = "text/plain"
+          message_body = "404:page not found"
+          status_code = 404
+        }
+    }
+    
+}
+
+resource "aws_lb_listener_rule" "asg" {
+    listener_arn = aws_lb_listener.http.arn
+    priority = 100
+
+    action {
+        type = "forward"
+        target_group_arn = aws_lb_target_group.asg.arn
+    }
+
+    condition {
+        path_pattern {
+            values = ["*"]
+        }
+    }
 }
